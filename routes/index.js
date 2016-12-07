a34a6029a786a4ae4f0f12814b005608e635bee3
Bryan Gardner <gardner.bryan22@gmail.com>
2016-12-07 11:44:50 -0500

    pass passport object to accounts route module

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const usersRoutes = require("./users");
 const itemsRoutes = require("./items");
-const newAccRoutes = require("./newAcc");
 const data = require("../data");
 
-const constructorMethod = (app) => {
+const constructorMethod = (app, passport) => {
+	const accRoutes = require("./accounts")(passport);
 	app.use("/users", usersRoutes);
 	app.use("/items", itemsRoutes);
-	app.use("/newaccount", newAccRoutes);
+	app.use("/account", accRoutes);
 
 	app.get("/", (req, res) => {
 		res.render("layouts/home", {});
