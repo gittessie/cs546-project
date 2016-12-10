6fc4b0a01aabb15b874f4c218f1b16e5a5ecb271
Bryan Gardner <gardner.bryan22@gmail.com>
2016-12-10 08:42:47 -0500

    delete user account

--- public/js/script.js
+++ public/js/script.js
@@ -28,4 +28,33 @@
         }
 
     })
+
+    var deleteAccountForm = $("#deleteAccount");
+    var userId = $("#userId");
+
+    deleteAccountForm.submit(function(event) {
+        event.preventDefault();
+
+        var  uid = userId.val();
+        console.log(userId);
+        if (uid) {
+
+            if (confirm("Are you sure you want to delete your account and associated items?")) {
+                var requestConfig = {
+                    method: "DELETE",
+                    url: "/account/" + uid,
+                    xhrFields: {
+                        withCredentials: true
+                    }
+                };
+
+                $.ajax(requestConfig).then(function(responseMessage) {
+                    location.href = responseMessage.redirect;
+                }, function(responseMessage) {
+                    alert("Delete Unsuccessful");
+                });
+            }
+        }
+
+    })
 })(window.jQuery, window.location);
